d5bc11db72013726cae280dbde0c51a3cbe162d3
nfwGytautas <gytautas.kk@gmail.com>
2022-10-29 23:14:40 +0300

    Correct code update and reload

--- examples/3 - Button onClick/src/App.cpp
+++ examples/3 - Button onClick/src/App.cpp
@@ -9,7 +9,12 @@
 #include "vex/ui/elements/Button.h"
 
 extern "C" {
+
 __attribute__((visibility("default"))) void button_onClick(vex::ui::elements::Button* btn) {
-    std::cout << btn->getSize().x << " " << btn->getSize().y;
+    std::cout << btn->getSize().x << " " << btn->getSize().y << "\n";
+}
+
+__attribute__((visibility("default"))) void button_onClick2(vex::ui::elements::Button* btn) {
+    std::cout << "Second callback\n";
 }
 }
